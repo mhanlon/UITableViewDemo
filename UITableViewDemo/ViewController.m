959871988ef3d22d9f59d302d0b3dceb65a07654
Matt Hanlon <mhanlon@gmail.com>
2015-05-14 20:35:11 -0700

    Initial import of the UITableViewDemo code.

--- UITableViewDemo/ViewController.m
+++ UITableViewDemo/ViewController.m
@@ -8,20 +8,153 @@
 
 #import "ViewController.h"
 
+static NSString* reuseableTableViewCellIdentifier = @"TableViewCell";
+
 @interface ViewController ()
+{
+    BOOL _isInsertingNewRestaurant;
+}
+@property (nonatomic, strong) UIButton *startInsertingButton;
+@property (nonatomic, strong) UIButton *addButton;
+@property (nonatomic, strong) UIView *footerView;
+
+@property (nonatomic, strong) NSArray *favoriteRestaurants;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (instancetype)init
+{
+    self = [super init];
+    if ( self )
+    {
+        self.favoriteRestaurants = @[@"Ronnie's Seafood, Charlton, Mass.", @"Ronnie's Seafood, Auburn, Mass.", @"The Oyster House", @"Legal Seafood", @"Five Guys"];
+        // A sort of more clear (and old) way of initializing this array would look like this:
+        // self.favoriteRestaurants = [NSArray arrayWithObjects:@"Ronnie's Seafood", @"Oyster House", @"Legal Seafood", @"Five Guys", nil];
+        _isInsertingNewRestaurant = NO;
+        self.addButton = [UIButton buttonWithType:UIButtonTypeContactAdd];
+        [self.addButton addTarget:self action:@selector(beginInsertingRestaurant:) forControlEvents:UIControlEventTouchUpInside];
+        
+        self.tableView.tableFooterView = self.addButton; // Lazily add our add button to the bottom of our table view
+                                                         // You'd want to make this look a bit nicer than this.
+        
+        // TODO: Make a view which shows off the behavior of adding an item at the end of the array and the insertion method
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)addRestaurant:(id)sender
+{
+    
+}
+
+- (void)beginInsertingRestaurant:(id)sender
+{
+    // Toggle our editing state
+    _isInsertingNewRestaurant = !_isInsertingNewRestaurant;
+    [self.tableView setEditing:_isInsertingNewRestaurant animated:NO];
+}
+
+#pragma mark -- UITableViewController delegate methods --
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.favoriteRestaurants.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:reuseableTableViewCellIdentifier];
+    if ( cell == nil )
+    {
+        // Initialize one if we don't have a re-usable one lying around, like the grocery bags you leave in the car and forget to take into the shop when you need them
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseableTableViewCellIdentifier];
+    }
+    
+    cell.textLabel.text = self.favoriteRestaurants[indexPath.row];
+    // A more clear (and old) way of writing this could look like this:
+    // cell.textLabel.text = [self.favoriteRestaurants objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark -- TableView Editing (Insertion, Deletion, Reordering) Methods --
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ( _isInsertingNewRestaurant )
+    {
+        return UITableViewCellEditingStyleInsert;
+    }
+    else
+    {
+        return UITableViewCellEditingStyleDelete;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ( editingStyle == UITableViewCellEditingStyleInsert )
+    {
+        // Code to handle insertion
+        _isInsertingNewRestaurant = NO; // Reset our editing state
+        NSMutableArray* mutableRestaurants = [NSMutableArray arrayWithArray:self.favoriteRestaurants];
+        NSInteger insertedRowPosition = ( indexPath.row + 1 );
+        [mutableRestaurants insertObject:@"Dunkin Donuts" atIndex:insertedRowPosition]; // We'll insert our new fave *after* our selected row
+
+        // TODO: Show an alert to ask the user for their favorite restaurant
+        // (which is probably still Dunkin Donuts, but it's worth checking)
+        // See https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/TableView_iPhone/ManageInsertDeleteRow/ManageInsertDeleteRow.html for an example
+        self.favoriteRestaurants = [NSArray arrayWithArray:mutableRestaurants];
+        
+        // The actual indexPath at which we insert the object is one up, so reload only that one
+        NSIndexPath* insertedIndexPath = [NSIndexPath indexPathForRow:insertedRowPosition inSection:indexPath.section];
+        [tableView insertRowsAtIndexPaths:@[insertedIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+    else if ( editingStyle == UITableViewCellEditingStyleDelete )
+    {
+        // Code to handle deletion
+        NSMutableArray* mutableRestaurants = [NSMutableArray arrayWithArray:self.favoriteRestaurants];
+        [mutableRestaurants removeObjectAtIndex:indexPath.row];
+        self.favoriteRestaurants = [NSArray arrayWithArray:mutableRestaurants];
+        
+        // Fade our rows out of sight our tableview
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+    [tableView setEditing:NO animated:YES];
+}
+
+#pragma mark -- Re-ordering your tableview rows --
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    NSMutableArray* mutableRestaurants = [NSMutableArray arrayWithArray:self.favoriteRestaurants];
+    id movingRestaurant = [mutableRestaurants objectAtIndex:sourceIndexPath.row];
+    [mutableRestaurants removeObjectAtIndex:sourceIndexPath.row];
+    [mutableRestaurants insertObject:movingRestaurant  atIndex:destinationIndexPath.row];
+    self.favoriteRestaurants = [NSArray arrayWithArray:mutableRestaurants];
 }
 
 @end
